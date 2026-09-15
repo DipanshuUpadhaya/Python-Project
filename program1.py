32fc619ffde55d5bba479af68392c1cab52ebf03
dipanshuupashyay123 <dipanshuupadhyay45@gmail.com>
2026-09-15 23:02:14 +0530

    Keep only File-Handling project

--- program1.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-def unique(lst):
-    counts=Counter(lst)
-    return [item for item,count in counts.items() if count==1]
-numbers=[1,2,3,2,4,5,1,6,3,7]
-print(unique(numbers))
--- statistics1.py
+++
@@ -1,8 +0,0 @@
-import statistics
-numbers=[2,4,6,8,10]
-mean=statistics.mean(numbers)
-median=statistics.median(numbers)
-mode=statistics.mode(numbers)
-print(f"Mean:{mean}")
-print(f"Median:{median}")
-print(f"Mode:{mode}")
